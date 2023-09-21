0a73b276e524739916f4a04c027e9114ee6a9bc7
Haider Khalil <haider.khalil@nkutechnologies.com>
2023-09-21 12:51:20 +0500

    Haider Khlil Chages in this Projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GanttChartComponent } from './gantt-chart/gantt-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'gantt-chart',component:GanttChartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GanttChartComponent } from './gantt-chart/gantt-chart.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      GanttChartComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
